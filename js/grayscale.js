fa660cb047cb2193de213bc46380d3911be53370
BryOrellana <00049318@uca.edu.sv>
2019-11-06 20:33:52 -0600

    [Add] Agregando css de la pagina

---
+++ js/grayscale.js
@@ -0,0 +1,132 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 100
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use stric
+
+
+//===========================LoginJS======================================
+
+(function ($) {
+  "use strict";
+
+
+  /*==================================================================
+  [ Focus input ]*/
+  $('.input100').each(function(){
+      $(this).on('blur', function(){
+          if($(this).val().trim() != "") {
+              $(this).addClass('has-val');
+          }
+          else {
+              $(this).removeClass('has-val');
+          }
+      })    
+  })
+
+
+  /*==================================================================
+  [ Validate ]*/
+  var input = $('.validate-input .input100');
+
+  $('.validate-form').on('submit',function(){
+      var check = true;
+
+      for(var i=0; i<input.length; i++) {
+          if(validate(input[i]) == false){
+              showValidate(input[i]);
+              check=false;
+          }
+      }
+
+      return check;
+  });
+
+
+  $('.validate-form .input100').each(function(){
+      $(this).focus(function(){
+         hideValidate(this);
+      });
+  });
+
+  function validate (input) {
+      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+              return false;
+          }
+      }
+      else {
+          if($(input).val().trim() == ''){
+              return false;
+          }
+      }
+  }
+
+  function showValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).removeClass('alert-validate');
+  }
+  
+  /*==================================================================
+  [ Show pass ]*/
+  var showPass = 0;
+  $('.btn-show-pass').on('click', function(){
+      if(showPass == 0) {
+          $(this).next('input').attr('type','text');
+          $(this).addClass('active');
+          showPass = 1;
+      }
+      else {
+          $(this).next('input').attr('type','password');
+          $(this).removeClass('active');
+          showPass = 0;
+      }
+      
+  });
+
+
+})(jQuery);
