1a5bca4a52de19d87247e666367fe1188c9721d9
BryOrellana <00049318@uca.edu.sv>
2019-11-09 21:54:06 -0600

    [Add] Agregando validaciones del login

--- js/login.js
+++ js/login.js
@@ -1,86 +1,31 @@
-(function ($) {
-    "use strict";
+let carnet_field = document.querySelector("#carnet_field");
+let password_field = document.querySelector("#password_field");
+let submit_btn = document.querySelector("#submit_btn");
+let link_btn = document.querySelector("#link")
+let carnet_regex = new RegExp('^[0-9]{8}$');
+//let carnetBD = metodo para pedir el valor del carnet en la base
+//let passBD = metodo para pedir el valor de la contraseña en la base
 
+submit_btn.addEventListener("click", () => {
+    let carnet = carnet_field.value
+    var url  = "index.html"
 
-    /*==================================================================
-    [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
-  
-  
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
     
-    /*==================================================================
-    [ Show pass ]*/
-    var showPass = 0;
-    $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
-            $(this).next('input').attr('type','text');
-            $(this).addClass('active');
-            showPass = 1;
-        }
-        else {
-            $(this).next('input').attr('type','password');
-            $(this).removeClass('active');
-            showPass = 0;
-        }
+
+    if (carnet_regex.test(carnet) && password_field.value == carnet_field.value) {
+        alert("Bienvenido :D ");
+        /*if(carnetBD == carnet.value && passBD == password_field.value) {
+            alert("Bienvenido :D ");
+        }*/
         
-    });
-
-
-})(jQuery);
+    } else {
+        if (carnet_field.value == "" || password_field.value == "") {
+            alert("campo de carnet o contraseña vacio");
+            event.preventDefault();
+        } else {
+        alert("Carnet o contraseña incorrecto")
+        event.preventDefault();
+        }
+    }
+})
+ 
