7723272bb5fc3816d1163d495c685180d44b0b09
OsAldana14 <00153818@uca.edu.sv>
2019-11-16 18:46:54 -0600

    Fix: Fixing Login

--- js/login.js
+++ js/login.js
@@ -10,48 +10,49 @@ let log;
 //let passBD = metodo para pedir el valor de la contraseña en la base
 
 submit_btn.addEventListener("click", () => {
-    let carnet = carnet_field.value
+            let carnet = carnet_field.value
 
-    if (carnet_regex.test(carnet) && password_field.value == carnet_field.value) {
-       // alert("Bienvenido :D ");
-        /*if(carnetBD == carnet.value && passBD == password_field.value) {
-            alert("Bienvenido :D ");
-        }*/
-        console.log("prueba")
-    var url = "index.html"
-
-
-    if (carnet_regex.test(carnet) && password_field.value == carnet_field.value) {
-        alert("Bienvenido " + carnet_field.value + " :D");
-
-        /*if(carnetBD == carnet.value && passBD == password_field.value) {
-            alert("Bienvenido :D ");
-        }*/
-        log=true;
-
-    } else {
-        if (carnet_field.value == "" || password_field.value == "") {
-            alert("campo de carnet o contraseña vacio");
-            event.preventDefault();
-        } else {
-            alert("Carnet o contraseña incorrecto")
-            event.preventDefault();
-        }
-
-        log = false;
-    }
-})
-window.addEventListener('keyup',(event)=>{
-    let keyCode = event.keyCode;
-    if(keyCode == 13){
-        submit_btn.click();
-    }
+            if (carnet_regex.test(carnet) && password_field.value == carnet_field.value) {
+                // alert("Bienvenido :D ");
+                /*if(carnetBD == carnet.value && passBD == password_field.value) {
+                    alert("Bienvenido :D ");
+                }*/
+                console.log("prueba")
+                var url = "index.html"
 
 
-    if (log == true) {
-        btnLogout.display = 'block';
-    } else {
-        
-    }
+                if (carnet_regex.test(carnet) && password_field.value == carnet_field.value) {
+                    alert("Bienvenido " + carnet_field.value + " :D");
 
-})
+                    /*if(carnetBD == carnet.value && passBD == password_field.value) {
+                        alert("Bienvenido :D ");
+                    }*/
+                    log = true;
+
+                } else {
+                    if (carnet_field.value == "" || password_field.value == "") {
+                        alert("campo de carnet o contraseña vacio");
+                        event.preventDefault();
+                    } else {
+                        alert("Carnet o contraseña incorrecto")
+                        event.preventDefault();
+                    }
+
+                    log = false;
+                }
+            }})
+
+        window.addEventListener('keyup', (event) => {
+            let keyCode = event.keyCode;
+            if (keyCode == 13) {
+                submit_btn.click();
+            }
+
+
+            if (log == true) {
+                btnLogout.display = 'block';
+            } else {
+
+            }
+
+        })
